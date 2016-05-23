929b8d7e43302826bb3a1eec7bb9b027207479cf
ElonGated <mr.elongated@gmail.com>
2016-05-23 13:59:49 -0500

    add user logging and activity logging, delete activity code is wonky, needs help

--- wellness-bot.js
+++ wellness-bot.js
@@ -1,456 +1,162 @@
-//require('dotenv').load();
+require('dotenv').load();
 var Botkit = require('botkit');
-//var redis = require('redis');
+var redis = require('redis');
 
 // connect to data
-/*var redisURL = process.env.REDIS_URL || 'redis://localhost:6379';
-var redisClient = redis.createClient(redisURL);
-*/
-var controller = Botkit.slackbot({
-    debug: true
-});
-
-// connect the bot to a stream of messages, token is saved in heroku config
-controller.spawn({
-    token: process.env.SLACK_INTEGRATION_TOKEN
-}).startRTM();
-
-// give the bot something to listen for.
-
-controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
-    //console.log(message);
-    bot.reply(message,"Hi Everybody!");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*//require('dotenv').load();
-var Botkit = require('botkit');
-//var axios = require('axios');
+var redisURL = process.env.REDIS_URL || 'redis://localhost:6379';
+var redisClient = redis.createClient();
 
 var controller = Botkit.slackbot({
     debug: true
 });
-console.log('before psot');
-axios.post('https://api.askfavor.com/api/v2/login.php',
-    {
-        email: 'test',
-        password: 'password'
-    })
-    .then(function(response) {
-        convo.say(JSON.stringify(response.body));
-    })
-    .catch(function(response) {
-        convo.say('Error');
-    });
-console.log('after post');
-// connect the bot to a stream of messages
+
+// connect the bot to a stream of messages, token is saved in .env file
 controller.spawn({
     token: process.env.SLACK_INTEGRATION_TOKEN
 }).startRTM();
 
+// connect to redis db and check connection
+redisClient.on('connect', function() {
+    console.log('redis is connected');
+});
+
+
 // give the bot something to listen for.
-
 controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
-    bot.reply(message,"Hello.");
+    console.log(message);
+    bot.reply(message, 'Hi Everybody!');
 });
 
-*/
-/*controller.hears('login', ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
-    bot.startConversation(message, startLoginConvo);
-});
-
-startLoginConvo = function (response, convo) {
-    promptEmail(response, convo, {key: 'email'});
-    convo.next();
-};
-*/
-/*promptEmail = function (response, convo) {
-    convo.ask('Please enter your email.', function (response, convo) {
-        promptPassword(response, convo, {key: 'password'});
-        convo.next();
-    });
-};
-
-promptPassword = function (response, convo) {
-    convo.ask('Please enter your password.', function (response, convo) {
-        console.log('Entered password');
-        console.log(convo.extractResponses());
-        var responses = convo.extractResponses();
-        login('test', 'test');
-    });
-};
-
-login = function(email, password) {
-    console.log('axios');
-    axios.post('https://api.askfavor.com/api/v2/login.php',
-        {
-            email: email,
-            password: password
-        })
-        .then(function(response) {
-            convo.say(JSON.stringify(response.body));
-        })
-        .catch(function(response) {
-            convo.say('Error');
-        });
-};*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-This is a sample Slack bot built with Botkit.
-This bot demonstrates many of the core features of Botkit:
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-# RUN THE BOT:
-  Get a Bot token from Slack:
-    -> http://my.slack.com/services/new/bot
-  Run your bot from the command line:
-    token=<MY TOKEN> node slack_bot.js
-# USE THE BOT:
-  Find your bot inside Slack to send it a direct message.
-  Say: "Hello"
-  The bot will reply "Hello!"
-  Say: "who are you?"
-  The bot will tell you its name, where it running, and for how long.
-  Say: "Call me <nickname>"
-  Tell the bot your nickname. Now you are friends.
-  Say: "who am I?"
-  The bot will tell you your nickname, if it knows one for you.
-  Say: "shutdown"
-  The bot will ask if you are sure, and then shut itself down.
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-# EXTEND THE BOT:
-  Botkit has many features for building cool and useful bots!
-  Read all about it here:
-    -> http://howdy.ai/botkit
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/*
-
-if (!process.env.token) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
-}
-
-var Botkit = require('./lib/Botkit.js');
-var os = require('os');
-
-var controller = Botkit.slackbot({
-    debug: true,
-});
-
-var bot = controller.spawn({
-    token: process.env.token
-}).startRTM();
-
-
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
+// sign up to 'users' data object
+controller.hears(['sign up', 'check in'],['direct_message'],function(bot,message) {
+    console.log(message);
+    redisClient.hget(message.user, 'name', function(err, reply) {
+        console.log(reply + ' this should be the user name');
+    
+        if (reply === message.user) {
+            bot.reply(message, 'Looks like you\'re already signed up.');
         } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
 
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
+            redisClient.hmset(message.user, {
+                'name': message.user,
+                'signuptime': message.ts,
+                'ogmessage': message.text
             });
-        }
+            bot.reply(message, {
+                text: 'You got it Dude!',
+                username: 'wellness-bot',
+                icon_emoji: ':yougotitdude:',
+            });
+        };
     });
 });
 
+// unsubscribe
+controller.hears(['unsubscribe'],['direct_message'],function(bot,message) {
+    console.log(message);
+    redisClient.hget(message.user, 'name', function(err, reply){
 
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+        if (reply != message.user) {
+            bot.reply(message, 'I don\'t think you\'re on my list. If you\'d like to subscribe say \'sign up\'.');
+        } else {
+            redisClient.del(message.user);
+            bot.reply(message, 'Farewell.');
+        };
+    });
 
-    bot.startConversation(message, function(err, convo) {
+});
 
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
+
+// add to activity list
+var activity = '';
+controller.hears(['activity', 'add'], ['direct_message'],function(bot,message) {
+    console.log(message);
+    if (message.text) {
+        var activity = message.text.slice(4);
+    }
+    redisClient.rpush(['activitylist', activity], function(err, reply) {
+        console.log(reply);
+    });
+    bot.reply(message, 'Sounds like fun!');
+});
+
+// display the activity list, shifted by 1 for humans
+controller.hears(['list', 'activities'], ['direct_message'],function(bot,message){
+    console.log(message);
+    redisClient.lrange('activitylist', 0, -1, function(err,reply) {
+        for(var i in reply){
+            var number = Number(i) + 1;
+            bot.reply(message, number + '. ' + reply[i]);
+        }
+    });
+
+});
+
+//remove from activity list
+controller.hears(['delete'], ['direct_message'],function(bot,message){
+    console.log(message);
+    bot.say(message, 'Ok, here\'s the list:');
+        // display ordered list
+        redisClient.lrange('activitylist', 0, -1, function(err,reply) {
+            for(var i in reply){
+                var number = Number(i) + 1;
+                bot.reply(message, number + '. ' + reply[i]);
             }
-        }
-        ]);
+        });
+        // ask what to delete
+    bot.startConversation(message, function(err, convo) {
+        convo.ask('What\'s the number of the activity you want me to delete?', function(response, convo) {
+            var responseNumber = Number(response);
+            redisClient.lrange('activitylist', responseNumber, function(err, reply) {
+                convo.ask('Sure you want to remove: ' + reply + ' ?', [
+                {
+                    pattern: bot.utterances.yes,
+                    callback: function(response, convo) {
+                        convo.say('Yeah, I never liked that one either.')
+                        redisClient.lrem('activitylist', String(reply));
+                        convo.next();
+                    }
+                },
+                {
+                    pattern: bot.utterances.no,
+                    callback: function(response, convo) {
+                        convo.say('Oh, ok I\'ll leave it in then.')
+                        convo.next();
+                    }
+                }
+                ]);
+            });
+        convo.next();
+        });
     });
 });
 
 
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-    });
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}  
-
-*/
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-This is a sample Slack bot built with Botkit.
-This bot demonstrates many of the core features of Botkit:
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Send a message with attachments
-* Send a message via direct message (instead of in a public channel)
-# RUN THE BOT:
-  Get a Bot token from Slack:
-    -> http://my.slack.com/services/new/bot
-  Run your bot from the command line:
-    token=<MY TOKEN> node demo_bot.js
-# USE THE BOT:
-  Find your bot inside Slack to send it a direct message.
-  Say: "Hello"
-  The bot will reply "Hello!"
-  Say: "Attach"
-  The bot will send a message with a multi-field attachment.
-  Send: "dm"
-  The bot will reply with a direct message.
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-# EXTEND THE BOT:
-  Botkit has many features for building cool and useful bots!
-  Read all about it here:
-    -> http://howdy.ai/botkit
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/*
-var Botkit = require('../lib/Botkit.js');
 
 
-if (!process.env.token) {
-  console.log('Error: Specify token in environment');
-  process.exit(1);
+
+
+
+
+
+
+
+
+
+
+
+/*var makeLemonade = function(glasses) {
+    return glasses * 2;
 }
 
-var controller = Botkit.slackbot({
- debug: false
-});
 
-controller.spawn({
-  token: process.env.token
-}).startRTM(function(err) {
-  if (err) {
-    throw new Error(err);
-  }
-});
-
-
-controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
-    bot.reply(message,"Hello.");
-});
-
-controller.hears(['attach'],['direct_message','direct_mention'],function(bot,message) {
-
-  var attachments = [];
-  var attachment = {
-    title: 'This is an attachment',
-    color: '#FFCC99',
-    fields: [],
-  };
-
-  attachment.fields.push({
-    label: 'Field',
-    value: 'A longish value',
-    short: false,
-  });
-
-  attachment.fields.push({
-    label: 'Field',
-    value: 'Value',
-    short: true,
-  });
-
-  attachment.fields.push({
-    label: 'Field',
-    value: 'Value',
-    short: true,
-  });
-
-  attachments.push(attachment);
-
-  bot.reply(message,{
-    text: 'See below...',
-    attachments: attachments,
-  },function(err,resp) {
-    console.log(err,resp);
-  });
-});
-
-controller.hears(['dm me'],['direct_message','direct_mention'],function(bot,message) {
-  bot.startConversation(message,function(err,convo) {
-    convo.say('Heard ya');
-  });
-
-  bot.startPrivateConversation(message,function(err,dm) {
-    dm.say('Private reply!');
-  });
-
-});
-
-
+makeLemonade(2);
 */
+
+
+
+
+
 
 
 
